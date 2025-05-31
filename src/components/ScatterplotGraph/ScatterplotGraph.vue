8313582de19a51141a3f84a9d125c26056ff9b5c
abraham96ce <45831981+abraham96ce@users.noreply.github.com>
2025-05-30 21:41:04 -0700

    Update

--- src/components/ScatterplotGraph/ScatterplotGraph.vue
+++ src/components/ScatterplotGraph/ScatterplotGraph.vue
@@ -11,11 +11,25 @@
 </template>
 
 <script setup lang="ts">
+// Importa funciones reactivas y de ciclo de vida desde Vue
 import { ref, onMounted } from 'vue'
+// Importa la función que construye el gráfico con D3
 import { drawChart } from './ScatterplotGraph'
 
+// Crea una referencia al elemento SVG donde se dibujará el gráfico.
+// Se usa ref para poder acceder al elemento del DOM una vez montado.
 const svgRef = ref<SVGSVGElement | null>(null)
 
+/**
+ * Hook del ciclo de vida `onMounted`.
+ * 
+ * Este hook se ejecuta automáticamente cuando el componente ha sido montado
+ * en el DOM. Es el momento adecuado para iniciar lógica que dependa del DOM,
+ * como operaciones con D3.js.
+ * 
+ * En este caso, `drawChart(svgRef.value)` se llama justo después de que el
+ * componente se ha montado, garantizando que el SVG esté presente y accesible.
+ */
 onMounted(() => {
     drawChart(svgRef.value)
 })
